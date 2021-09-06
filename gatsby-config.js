1f3cbd6a2dd949c257be0ef2572560f2e190b168
Brandon Smith <6439108-bsdev23@users.noreply.gitlab.com>
2021-09-06 10:14:32 -0700

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,42 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "JusAte",
+    description: "Find the best food around you",
+    author: "@thesamebs",
+    person: { name: "Brandon", age: 32 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "Brandon", age: 32 },
+      { name: "jehan", age: 33 },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `n7aes6ozdyv8`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,27 @@
 import React from "react"
-
+// import { Link } from "gatsby"
+import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>jusAte</h1>
+              <h4>Find plates Just Ate around you</h4>
+            </div>
+          </div>
+        </header>
+      </main>
+    </Layout>
+  )
 }
